a6b9fb3b0f32581905fad9b8b502541bc0a31f83
namitamanohar <manohar.namita@gmail.com>
2020-04-27 15:41:27 -0500

    build error

--- Bangazon/Controllers/PaymentTypesController.cs
+++ Bangazon/Controllers/PaymentTypesController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Bangazon.Data;
 using Bangazon.Models;
-using Bangazon.Models.PaymentTypeViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
