b4fcd17756829c50ecb17bf1a055410bb6d6f431
namitamanohar <manohar.namita@gmail.com>
2020-04-27 11:19:08 -0500

    adding product, dropdown for categories and max value for price and no special characters

---
+++ Bangazon/Controllers/ProductsController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bangazon.Data;
+using Bangazon.Models;
+using Bangazon.Models.ProductViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bangazon.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProductsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+
+            _context = context;
+            _userManager = userManager;
+        }
+        // GET: Products
+        public async Task<ActionResult> Index()
+        {
+            var user = await GetCurrentUserAsync();
+            var products = await _context.Product
+                .Where(p => p.UserId == user.Id)
+                .Include(p => p.ProductType)
+                .ToListAsync(); 
+
+            return View(products);
+        }
+
+        // GET: Products/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Products/Create
+        public async Task<ActionResult> Create()
+        {
+            var ProductTypeOptions = await _context.ProductType
+              .Select(pt => new SelectListItem() { Text = pt.Label , Value = pt.ProductTypeId.ToString() })
+              .ToListAsync();
+
+            var viewModel = new ProductFormViewModel();
+
+            viewModel.ProductTypeOptions = ProductTypeOptions; 
+
+
+            return View(viewModel);
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProductFormViewModel productViewItem)
+        {
+            try
+            {
+                var user = await GetCurrentUserAsync(); 
+
+                var product = new Product
+                {
+                    DateCreated = productViewItem.DateCreated, 
+                    Description = productViewItem.Description, 
+                    Title = productViewItem.Title, 
+                    Price = productViewItem.Price, 
+                    Quantity = productViewItem.Quantity, 
+                    UserId = user.Id, 
+                    City = productViewItem.City, 
+                    Active = productViewItem.Active, 
+                    ProductTypeId = productViewItem.ProductTypeId
+
+                    
+                };
+
+                _context.Product.Add(product);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            var item = await _context.Product.Include(p => p.ProductType).FirstOrDefaultAsync(p => p.ProductId == id);
+            return View(item);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id, Product product)
+        {
+            try
+            {
+                _context.Product.Remove(product); 
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+    }
+}
---
+++ Bangazon/Models/ProductViewModels/ProductFormViewModel.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProductViewModels
+{
+    public class ProductFormViewModel
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [RegularExpression(@"^[a-zA-Z0-9'' '.]+$", ErrorMessage = "Special character should not be entered")]
+        public string Description { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9'' ']+$", ErrorMessage = "Special character should not be entered")]
+        [StringLength(55, ErrorMessage = "Please shorten the product title to 55 characters")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [Range(typeof(double), "0.01", "10000.00", ErrorMessage = "Price must be be at least $0.01 and not greater than $10,000.00")]
+
+
+        public double Price { get; set; } 
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public string City { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public bool Active { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        [Display(Name = "Product Category")]
+        public int ProductTypeId { get; set; }
+
+        public ProductType ProductType { get; set; }
+
+        public List<SelectListItem> ProductTypeOptions { get; set; }
+        public virtual ICollection<OrderProduct> OrderProducts { get; set; }
+
+        public ProductFormViewModel()
+        {
+            Active = true;
+        }
+
+     
+
+    }
+}
